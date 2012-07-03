5f9efb304b73b1264b9dc02a03d38f5c472e9c75
Mauricio Mantish <mauriciomantilla@gmail.com>
2012-07-03 15:26:42 -0500

    atributos de wordpress en <head>

--- wp-content/themes/congresoifx/index.php
+++ wp-content/themes/congresoifx/index.php
@@ -2,12 +2,12 @@
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-	<meta charset="utf-8">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>II Congreso Internacional deTraders Profesionales Data IFX</title>
+	<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
 	<meta name="description" content="El II Congreso Internacional de Traders Profesionales Data IFX ha sido diseñado para ofrecerles herramientas enriquecedoras en temas concernientes al manejo eficáz de inversiones.">
 	<meta name="author" content="http://www.dataifx.com">
 
@@ -15,9 +15,12 @@
 
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 
-	<script src="js/libs/modernizr-2.5.3.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/js/libs/modernizr-2.5.3.min.js"></script>
+<?php
+	wp_head();
+?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <!--[if lt IE 7]><p class=chromeframe>¡Su navegador es <em>muy antiguo!</em> <a href="http://browsehappy.com/">Mejore su experiencia en la web actualizando su navegador</a> ó <a href="http://www.google.com/chromeframe/?redirect=true">instale Google Chrome Frame</a> para navegar este sitio.</p><![endif]-->
 <div id="wrapper_main">
 <header id="header_main">
