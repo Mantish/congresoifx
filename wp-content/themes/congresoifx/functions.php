3f4ec4d0d0060ac2df5364fce5096c97419eba2d
Mauricio Mantish <mauriciomantilla@gmail.com>
2012-07-05 08:09:06 -0500

    soporte para post thumbnails. Tipo de post: Rotador

--- wp-content/themes/congresoifx/functions.php
+++ wp-content/themes/congresoifx/functions.php
@@ -1,8 +1,38 @@
 <?php
+add_theme_support( 'post-thumbnails' );
+
 //jquery desde google cdn
-function my_jquery() {
+function my_jquery()
+{
 	wp_deregister_script( 'jquery' );
 	wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', false, '1.7.2', true);
 	wp_enqueue_script( 'jquery' );
 }
 add_action('wp_enqueue_scripts', 'my_jquery');
+
+// Registrar post types
+function create_post_types()
+{
+	register_post_type( 'slide', array(
+		'labels' => array(
+			'name' => 'Rotadores',
+			'singular_name' => 'Rotador',
+			'add_new' => 'Añadir Nuevo',
+			'add_new_item' => 'Añadir Nuevo Rotador',
+			'edit_item' => 'Editar Rotador',
+			'new_item' => 'Nuevo Rotador',
+			'view_item' => 'Ver Rotador',
+			'search_items' => 'Buscar Rotadores',
+			'not_found' => 'Rotadores no encontrados',
+			'not_found_in_trash' => 'Rotadores no encontrados en papelera'
+		),
+		'public' => true,
+		'supports' => array(
+			'title',
+			'thumbnail',
+			'page-attributes',
+		),
+		'menu_position' => 21,
+	));
+}
+add_action('init', 'create_post_types');
