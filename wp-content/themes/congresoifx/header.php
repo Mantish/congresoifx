89f722ca9eed968c6075ef1f55eefb0aa3cc56b0
Mauricio Mantish <mauriciomantilla@gmail.com>
2012-07-05 10:30:14 -0500

    rotador movido al template header

--- wp-content/themes/congresoifx/header.php
+++ wp-content/themes/congresoifx/header.php
@@ -38,3 +38,23 @@
 <li id="ultimo"><a href="contacto.html">CONTACTO</a></li>
 </ul>
 </nav><!-- termina #nav_main-->
+
+<div class="contenedor_rotador">
+	<div id="slides">
+		<div class="slides_container">
+		<?php
+		$args = array( 'numberposts' => -1, 'orderby' => 'menu_order', 'order' => 'ASC', 'post_type' => 'slide' );
+		$query_slider = new WP_Query($args);
+		while ( $query_slider->have_posts() ) : $query_slider->the_post();
+		?>
+			<a href="#" title="<?php the_title(); ?>"><?php the_post_thumbnail('full'); ?></a>
+		<?php
+		endwhile;
+		wp_reset_postdata();
+		?>
+		</div><!-- termina .slides_container-->
+	</div><!-- termina #slides-->
+
+	<aside class="partner"><img src="<?php echo get_bloginfo('template_directory');?>/img/img_partner.jpg" width="407" height="174" alt="partner_seix_capital_humano"></aside>
+	<div class="clear"></div>
+</div><!-- termina .contenedor_rotador-->
--- wp-content/themes/congresoifx/index.php
+++ wp-content/themes/congresoifx/index.php
@@ -1,24 +1,5 @@
 <?php get_header(); ?>
 
-<div class="contenedor_rotador">
-	<div id="slides">
-		<div class="slides_container">
-		<?php
-		$args = array( 'numberposts' => -1, 'orderby' => 'menu_order', 'order' => 'ASC', 'post_type' => 'slide' );
-		$query_slider = new WP_Query($args);
-		while ( $query_slider->have_posts() ) : $query_slider->the_post();
-		?>
-			<a href="#" title="<?php the_title(); ?>"><?php the_post_thumbnail('full'); ?></a>
-		<?php
-		endwhile;
-		wp_reset_postdata();
-		?>
-		</div><!-- termina .slides_container-->
-	</div><!-- termina #slides-->
-
- <aside class="partner"><img src="<?php echo get_bloginfo('template_directory');?>/img/img_partner.jpg" width="407" height="174" alt="partner_seix_capital_humano"></aside>
-<div class="clear"></div>
-</div><!-- termina .contenedor_rotador-->
 <div class="columna_izquierda">
 <article id="beneficios">
 <h1>BENEFICIOS</h1>
