b7ec663c37b01195b74118814f2e39dfd8e9b6fd
Mauricio Mantish <mauriciomantilla@gmail.com>
2012-07-16 10:51:07 -0500

    slider tambien movido a su propio template (solo para paginas)

---
+++ wp-content/themes/congresoifx/slider-pages.php
@@ -0,0 +1,20 @@
+<div class="contenedor_rotador">
+	<div id="slides">
+		<div class="slides_container">
+		<?php
+		$args = array( 'numberposts' => -1, 'orderby' => 'menu_order', 'order' => 'ASC', 'post_type' => 'slide', 'connected_type' => 'slides_to_pages', 'connected_items' => get_queried_object() );
+		$query_slider = new WP_Query($args);
+		while ( $query_slider->have_posts() ) : $query_slider->the_post();
+			$link = get_post_meta( get_the_ID(), '_slide_link', true );
+		?>
+			<a href="<?php echo $link; ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('full'); ?></a>
+		<?php
+		endwhile;
+		wp_reset_postdata();
+		?>
+		</div><!-- termina .slides_container-->
+	</div><!-- termina #slides-->
+
+	<aside class="partner"><img src="<?php echo get_bloginfo('template_directory');?>/img/img_partner.jpg" width="407" height="174" alt="partner_seix_capital_humano"></aside>
+	<div class="clear"></div>
+</div><!-- termina .contenedor_rotador-->
